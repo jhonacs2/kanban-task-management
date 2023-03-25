f3a340d4d7ab91b2aa776bcabe3e406d8236da72
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-03-24 22:42:18 -0400

    init structure

---
+++ src/app/routes/app-routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: 'public',
+    loadChildren: () => import('../modules/public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: 'secure',
+    loadChildren: () => import('../modules/secure/secure.module').then(m => m.SecureModule)
+  },
+  {
+    path: '',
+    redirectTo: 'public',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'public'
+  }
+];
